c0bec3ff35e0b639b6c8633233e725fcbb2fdc6a
Xavier Boubert <xavier@boubert.fr>
2016-08-23 16:23:53 +0200

    fix(express bootstrap server): add logs

--- features/express/express-bootstrap-server.js
+++ features/express/express-bootstrap-server.js
@@ -5,7 +5,11 @@ module.exports = function($allonsy, callback) {
   var path = require('path'),
       async = require('async');
 
+  $allonsy.log('allons-y-express', 'express-bootstrap-start');
+
   async.mapSeries($allonsy.findInFeaturesSync('*-express-bootstrap.js'), function(file, nextFile) {
+
+    $allonsy.log('allons-y-express', 'express-bootstrap-exec:' + file);
 
     DependencyInjection.injector.controller.invoke(null, require(path.resolve(file)), {
       controller: {
