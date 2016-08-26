4f3e3c8449d9b36596298703feac59e718b3488d
Xavier Boubert <xavier@boubert.fr>
2016-08-26 10:05:02 +0200

    feat(express allons-y-start): support "cpu" config

--- features/express/express-allons-y-start.js
+++ features/express/express-allons-y-start.js
@@ -6,7 +6,9 @@ module.exports = {
   name: 'Allons-y Express',
   enabled: !process.env.EXPRESS || process.env.EXPRESS == 'true' || false,
   fork: true,
-  forkCount: parseInt(process.env.EXPRESS_COUNT || 1, 10),
+  forkCount: process.env.EXPRESS_COUNT && process.env.EXPRESS_COUNT == 'cpu' ?
+    require('os').cpus().length :
+    parseInt(process.env.EXPRESS_COUNT || 1, 10),
   forkMaxRestarts: parseInt(process.env.EXPRESS_RESTARTS || 1, 10),
   watch: '*-express.js',
   module: require(path.resolve(__dirname, 'express-server.js'))
