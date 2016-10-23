948457812c2958f77e84d5c82f8b289d7e59d50b
Xavier Boubert <xavier@boubert.fr>
2016-10-23 18:02:58 +0200

    feat(express service back): add {port} and {index} variables to EXPRESS_URL

--- features/express/models/express-service-back.js
+++ features/express/models/express-service-back.js
@@ -12,9 +12,13 @@ module.exports = function() {
           return _processes[p.id];
         }
 
+        var index = p.id.split('.')[1];
+
         _processes[p.id] = _processes[p.id] || {};
         _processes[p.id].port = port;
-        _processes[p.id].url = (process.env.EXPRESS_URL || '') + ':' + port;
+        _processes[p.id].url = process.env.EXPRESS_URL
+          .replace(/{port}/g, port)
+          .replace(/{index}/g, index);
 
         return _processes[p.id];
       };
